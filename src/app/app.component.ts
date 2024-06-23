9c710b86b0c6d4ce85cd6107a9c9b609af2de80b
jhonatan <jhonatan.s.costa97@gmail.com>
2024-06-23 15:39:13 -0300

    feat(app): moved the deep links logic to a deep link service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
-import {DeeplinkMatch, Deeplinks} from '@awesome-cordova-plugins/deeplinks/ngx';
-import {NavController, Platform} from '@ionic/angular';
+import {Platform} from '@ionic/angular';
+import {DeepLinksService} from './services/deep-links.service';
 
 @Component({
   selector: 'app-root',
@@ -14,49 +14,20 @@ export class AppComponent {
   constructor(
     private router: Router,
     private platform: Platform,
-    private deepLinks: Deeplinks,
-    private navControl: NavController,
+    private deepLinksService: DeepLinksService
   ) {
     this.initializedApp();
+  }
+
+  private initializedApp(): void {
+    this.platform.ready().then((): void => {
+      this.deepLinksService.initDeepLinks();
+    });
   }
 
   showCarFooterNavMenu(): boolean {
     const currentUrl: string = this.router.url.split('/')[1];
     return this.allowCarFooterMenuRoutes.some((route: string): boolean => currentUrl === route);
-  }
-
-  private initializedApp(): void {
-    this.platform.ready().then((): void => {
-      this.initDeepLinks();
-    });
-  }
-
-  private initDeepLinks() {
-    this.deepLinks.route({
-      '/redefine-password': {}
-    }).subscribe({
-      next: (match: DeeplinkMatch): void => {
-        this.redirectDeepLinkRoute(match.$route);
-      }, error: noMatch => {
-        console.error('Nenhum deep link encontrado para a rota passada', noMatch);
-      }
-    });
-  }
-
-  redirectDeepLinkRoute(routeUrl: 'redefine-password'): void {
-    switch (routeUrl) {
-      case 'redefine-password':
-        this.redirectRoute(routeUrl);
-        break;
-      default:
-        console.error('Rota de deep link não encontrada:', routeUrl);
-        this.redirectRoute('splash-screen-in');
-        break;
-    }
-  }
-
-  redirectRoute(url: string): void {
-    void this.navControl.navigateForward(url);
   }
 
 }
---
+++ src/app/services/deep-links.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {DeeplinkMatch, Deeplinks} from '@awesome-cordova-plugins/deeplinks/ngx';
+import {NavController} from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeepLinksService {
+
+  constructor(
+    private deepLinks: Deeplinks,
+    private navControl: NavController,
+  ) { }
+
+  public initDeepLinks(): void {
+    this.deepLinks.route({
+      '/redefine-password': {}
+    }).subscribe({
+      next: (match: DeeplinkMatch): void => {
+        this.redirectDeepLinkRoute(match.$route);
+      }, error: noMatch => {
+        console.error('Nenhum deep link encontrado para a rota passada', noMatch);
+      }
+    });
+  }
+
+  private redirectDeepLinkRoute(routeUrl: 'redefine-password'): void {
+    switch (routeUrl) {
+      case 'redefine-password':
+        this.redirectRoute(routeUrl);
+        break;
+      default:
+        console.error('Rota de deep link não encontrada:', routeUrl);
+        this.redirectRoute('splash-screen-in');
+        break;
+    }
+  }
+
+  private redirectRoute(url: string): void {
+    void this.navControl.navigateForward(url);
+  }
+}
